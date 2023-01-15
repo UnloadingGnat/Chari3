7017d1027407ef72e87b25dcb2f4865c85d3399c
UnloadingGnat <jaavinjm123@gmail.com>
2023-01-15 01:07:28 -0500

    Finished Minter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,107 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 
-import CreateCampaign from './pages/CreateCampaign';
+import { useState } from 'react';
+import axios from 'axios';
+import FormData from 'form-data';
+
 
 import { ethers } from 'ethers';
 import { Web3Button } from "@thirdweb-dev/react";
 
 import { useContract, useContractRead } from "@thirdweb-dev/react";
+
+
+const API_KEY = 'sk_live_30dac2d8-19ff-447a-bbe9-24e6a5b4650e';
+
+export function ImageUpload() {
+  const [filePath, setFilePath] = useState(null);
+  const [allowPlatformToOperateToken, setAllowPlatformToOperateToken] = useState('');
+  const [chain, setChain] = useState('');
+  const [recipientAddress, setRecipientAddress] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [response, setResponse] = useState(null);
+
+  const handleFileUpload = (e) => {
+    setFilePath(e.target.files[0]);
+  }
+  
+  const handleSubmit = (e) => {
+    const form = new FormData();
+    form.append('allowPlatformToOperateToken', 'true');
+    form.append('chain', 'goerli');
+    form.append('name', 'test123');
+    data.append('filePath', filePath);
+    form.append('description', '123');
+    form.append('recipientAddress', '0x372f66206a0f92A2eF3529a3D800Cbe363bfd800');
+    form.append('data', '0.000123');
+
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'X-API-Key': 'sk_live_30dac2d8-19ff-447a-bbe9-24e6a5b4650e'
+      }
+    };
+
+    options.body = form;
+
+    fetch('https://api.verbwire.com/v1/nft/mint/quickMintFromFile', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }
+
+  return (
+    <div className="form-container">
+      <h1>Chari3 NFT Minter w/ Verbwire API</h1>
+      <h3>Incentivize People to Donate to your organization by rewarding them with NFTS!</h3>
+      <form onSubmit={handleSubmit}>
+        <input className="form-input" type="file" onChange={handleFileUpload} />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="allowPlatformToOperateToken"
+          value={allowPlatformToOperateToken}
+          onChange={e => setAllowPlatformToOperateToken(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="chain"
+          value={chain}
+          onChange={e => setChain(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="recipientAddress"
+          value={recipientAddress}
+          onChange={e => setRecipientAddress(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="description"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button className="form-button" type="submit">Upload</button>
+      </form>
+      {response && <div>{JSON.stringify(response)}</div>}
+    </div>
+    
+);
+}
+
 
 
 export function Donate() {
@@ -75,8 +170,8 @@ Introducing the first ever Decentralized Web3 Charity Solution
       <div className='create'>
         <form class="form-container">
         <div className='title'>Create a Chari3!</div>
-        <div className='description'>A Chari3 is deployed on the blockchain where all donations are PUBLIC</div>
-        <div className='forms'>
+        <div className='description-create'>A Chari3 is deployed on the blockchain where all donations are PUBLIC</div>
+        <div className='forms-create'>
             <div class="form-group">
               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter a charity name" />
             </div>
@@ -90,6 +185,9 @@ Introducing the first ever Decentralized Web3 Charity Solution
          
         </div>
         </form>
+        <div className='footer'></div>
+        <div className='footer'></div>
+
 
       </div>
       <div className='donate'>
@@ -108,6 +206,7 @@ Introducing the first ever Decentralized Web3 Charity Solution
               <div className='buttoncreate'>
                 <Donate />
               </div>
+              
           
           </div>
 
@@ -116,8 +215,15 @@ Introducing the first ever Decentralized Web3 Charity Solution
 
       </div>
       <div className='footer'></div>
-
+      <div className='footer'></div>
+      <div className='footer'></div>
+      <div>
+        <ImageUpload />
       </div>
+      <div className='footer'></div>
+
+
+    </div>
   )
 }
 
